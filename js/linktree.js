587f85d7047df91b25fac5116d1a46ee0ff8b745
satriairawan05 <satriai418@gmail.com>
2023-10-31 12:28:31 +0800

    Fix Typing

--- js/linktree.js
+++ js/linktree.js
@@ -4,37 +4,14 @@ function animateTyping() {
     var text = element.innerHTML;
     element.innerHTML = "";
     var i = 0;
-    var isDeleting = false;
 
     function type() {
         if (i < text.length) {
-            isDeleting = false;
             element.innerHTML += text.charAt(i);
             i++;
             setTimeout(type, Math.floor(Math.random() * 200) + 100); // Waktu penundaan antara pengetikan karakter yang berbeda
         } else {
-            isDeleting = true;
-            setTimeout(resetTyping, 1000); // Waktu penundaan sebelum mereset efek pengetikan
-        }
-
-        var currentText = text.substring(0, i);
-
-        if (isDeleting) {
-            element.innerHTML = currentText;
-            i--;
-        } else {
-            element.innerHTML = currentText + text.charAt(i);
-            i++;
-        }
-
-        if (i <= 0) {
-            isDeleting = false;
-            setTimeout(type, Math.floor(Math.random() * 200) + 100); // Waktu penundaan sebelum mulai mengetik lagi
-        } else if (i === text.length) {
-            isDeleting = true;
-            setTimeout(animateTyping, 2000); // Waktu penundaan sebelum memulai kembali efek pengetikan
-        } else {
-            setTimeout(type, Math.floor(Math.random() * 200) + 100); // Waktu penundaan antara pengetikan karakter yang berbeda
+            setTimeout(resetTyping, 5000); // Waktu penundaan sebelum mereset efek pengetikan
         }
     }
 
