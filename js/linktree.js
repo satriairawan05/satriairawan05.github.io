ee47d2343854724c7b184854ff06f4ddf3070e5c
satriairawan05 <satriai418@gmail.com>
2023-10-31 11:33:47 +0800

    Add Animate Typing

---
+++ js/linktree.js
@@ -0,0 +1,27 @@
+// JavaScript untuk mengulang efek pengetikan
+function animateTyping() {
+    var element = document.querySelector('.typing');
+    var text = element.innerHTML;
+    element.innerHTML = '';
+    var i = 0;
+
+    function type() {
+      if (i < text.length) {
+        element.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, Math.floor(Math.random() * 200) + 100); // Waktu penundaan antara pengetikan karakter yang berbeda
+      } else {
+        setTimeout(resetTyping, 5000); // Waktu penundaan sebelum mereset efek pengetikan
+      }
+    }
+
+    function resetTyping() {
+      element.innerHTML = '';
+      i = 0;
+      setTimeout(type, 0); // Mulai lagi efek pengetikan tanpa henti
+    }
+
+    type();
+  }
+
+  animateTyping();
